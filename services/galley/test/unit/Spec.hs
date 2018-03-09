3cf777395dd139bb58897813942b9188c0ae73f0
Jon Elverkilde <jon@wire.com>
2018-03-09 12:02:15 +0100

    WIP: add test stub, split conversation creation

---
+++ services/galley/test/unit/Spec.hs
@@ -0,0 +1,13 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain $
+    testCase "Example test case" $ do
+    -- assertion no. 1 (passes)
+    2 + 2 @?= 4
+    -- assertion no. 2 (fails)
+    assertBool "the list is not empty" $ null [1]
+    -- assertion no. 3 (would have failed, but won't be executed because
+    -- the previous assertion has already failed)
+    "foo" @?= "bar"
